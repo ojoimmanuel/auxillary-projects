3a7dbcefaffeca5663d0d131e7943aed07451d50
ojoimmanuel <ojoimmanuel@gmail.com>
2023-01-22 17:10:10 +0100

    aux proj

---
+++ onboarding_users.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+
+# SCRIPT TO ONBOARD NEW USERS LISTED ON THE names.csv FILE
+
+# CHECK IF names.csv FILE DOES NOT EXIST
+if [ -e "$(ls names.csv)" ] 
+then
+    echo ""
+else
+    echo "file is missing"
+fi
+
+# setting variables
+
+line=1
+groupname=developers
+password=nnnnnn
+
+
+
+function onboard_user(){
+    echo "Onboarding user $1..."
+    # check if user exists
+    if [ $(getent passwd $1) ]
+    then
+        echo "user $1 already exists."
+    else
+        # create user and add to group
+        sudo useradd -m -G $groupname $1
+
+        #set password for user
+        sudo echo -e "$password\n$password" | sudo passwd "$1"
+
+        # check if .ssh exists. create if not
+        if [ -e "/home/$1/.ssh" ]
+        then
+            echo ""
+        else
+            sudo mkdir /home/$1/.ssh
+            echo ".ssh directory created for user $1"
+        fi
+
+        # copy public key of current user to new user as authorized_keys
+        sudo cp ~/.ssh/id_rsa.pub /home/$1/.ssh/authorized_keys
+
+        echo "user $1 created successfully"
+    fi    
+
+    
+    echo ""
+    echo "--------------------------------"
+    echo ""
+}
+
+
+while read -r USER
+do 
+    onboard_user "$USER"
+    ((LINE++))
+done < "./names.csv"
